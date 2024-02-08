ab269429b75b51c498b9e2ed2a4c6cee0b752a5a
Ansh_Joshi <anshsjoshi@outlook.com>
2024-02-08 18:17:32 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express  = require("express")
+const http =  require('http');
+const {v4: uuidv4} = require('uuid');
+const cors = require('cors');
+const twilio = require('twilio');
+//?Defining PORT number here 
+const PORT = process.env.port || 5002;
+const app = express();
+const server = http.createServer(app);
+app.use(cors());
+const io= require('socket.io')(server,{
+    cors:{
+        origin : '*',
+        methods:['GET','POST']
+    }
+});
+
+server.listen(PORT,()=>{
+    console.log(`SERVER IS LISTINING AT ${PORT}`);
+})
